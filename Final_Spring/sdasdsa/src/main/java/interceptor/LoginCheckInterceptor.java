a4912318cfebb38f018c8f7710ff01c396a0ff52
tj3828 <tj3828@naver.com>
2019-02-14 20:43:47 +0900

    로그인 체크 관련 인터셉터

---
+++ Final_Spring/sdasdsa/src/main/java/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,25 @@
+package interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("id");
+		if(id == null || id.equals("")) {
+			response.sendRedirect("../login/login.do");
+			return false;
+		}
+		return true;
+	}
+	
+	
+}
