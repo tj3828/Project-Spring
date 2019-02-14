a6588ff9f60b7ad8993d42d2242c2e04de6c9b35
tj3828 <tj3828@naver.com>
2019-02-14 20:29:38 +0900

    Revert "로그인 처리 클래스"
    This reverts commit 6dd4c58de9c911c667d63fb0737f41a361d75b41.

--- Final_Spring/sdasdsa/src/main/java/com/hb/account/join/IJoinService.java
+++
@@ -1,9 +0,0 @@
-package com.hb.account.join;
-
-import com.hb.account.AccountVO;
-
-public interface IJoinService {
-		int checkId(AccountVO dto);
-		int checkNick(AccountVO dto);
-		void join(AccountVO dto);
-}
