a60fd2ac4f5a2eccad3ffd728466b572e5a5704b
tj3828 <tj3828@naver.com>
2019-02-18 02:39:33 +0900

    자유게시판 Reply VO (Valid 추가)

--- Final_Spring/sdasdsa/src/main/java/com/hb/freeboard/reply/FreeBoard_ReplyVO.java
+++ Final_Spring/sdasdsa/src/main/java/com/hb/freeboard/reply/FreeBoard_ReplyVO.java
@@ -8,13 +8,13 @@ import lombok.Data;
 @Data
 public class FreeBoard_ReplyVO {
 	private int num;
-	@NotBlank
+	@NotBlank(message="로그인을 해주세요.")
 	private String nickname;
-	@NotBlank
-	@Length(max=100)
+	@NotBlank(message="댓글을 입력해주세요.")
+	@Length(max=100,message="댓글은 100자이하여야 합니다.")
 	private String content;
 	private String writeDate;
-	@NotBlank
+	@NotBlank(message="비정상적인 접근입니다.")
 	private String fr_idx;
 	private String profile_img;
 		
