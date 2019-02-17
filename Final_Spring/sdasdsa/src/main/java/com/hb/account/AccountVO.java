cd92cbe5002e0ab1c1bc0c556edf2e06ecba1f91
tj3828 <tj3828@naver.com>
2019-02-18 02:35:07 +0900

    계정 VO (Vaild 적용)

--- Final_Spring/sdasdsa/src/main/java/com/hb/account/AccountVO.java
+++ Final_Spring/sdasdsa/src/main/java/com/hb/account/AccountVO.java
@@ -2,32 +2,33 @@ package com.hb.account;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
+
 @Data
 public class AccountVO {
-	@NotBlank
+	@NotBlank(message="아이디를 입력하세요.")
 	private String id;
-	@NotBlank
+	@NotBlank(message="비밀번호를 입력하세요.")
 	private String pw;
-	@NotBlank
+	@NotBlank(message="이름를 입력하세요.")
 	private String name;
-	@NotBlank
+	@NotBlank(message="닉네임을 입력하세요.")
 	private String nickname;
-	@NotBlank @Email
+	@NotBlank(message="이메일을 입력하세요.")
+	@Email(message="이메일 형식을 확인하세요.")
 	private String email;
-	@NotBlank
+	@NotBlank(message="전화번호를 입력하세요.")
 	private String phone;
-	@NotBlank
+	@NotBlank(message="주소를 입력하세요.")
 	private String addr1;
-	@NotBlank
+	@NotBlank(message="상세주소를 입력하세요.")
 	private String addr2;
-	@NotBlank
+	@NotBlank(message="성별을 선택하세요.")
 	private String gender;
-	@NotBlank
+	@NotBlank(message="프로필 이미지를 선택하세요.")
 	private String profile_img;
 	private MultipartFile multipartFile;
 	
