ff8a03e2154b55433af864825cc93cc727a4890e
tj3828 <tj3828@naver.com>
2019-02-23 00:59:41 +0900

    no message

--- Final_Spring/sdasdsa/src/main/java/com/hb/freeboard/like/ILikeService.java
+++
@@ -1,8 +0,0 @@
-package com.hb.freeboard.like;
-
-public interface ILikeService {
-	int checkLike(LikeVO ldto);
-	void deleteLike(LikeVO ldto);
-	void insertLike(LikeVO ldto);
-	int getLike(LikeVO ldto);
-}
